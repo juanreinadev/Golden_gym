09454c4e16211312b3066f4f949261f2d843c921
sebasAR <juan.reina.vz@gmail.com>
2026-05-15 22:40:47 -0500

    camcie un poco el css

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template("inicio.html")
+    return render_template("index.html")
 
 
 @app.route("/registro", methods=["GET", "POST"])
